5ff4ed78b6fafd41305e140728fed2d17afb1b05
thesilph <the_silph@hotmail.com>
2025-07-20 22:31:07 +0100

    deployable

---
+++ test/deploy.ts
@@ -0,0 +1,53 @@
+import {
+  time,
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
+import hre from "hardhat";
+import { getAddress, parseGwei } from "viem";
+
+  async function deployBankie() {
+    const adoptionPrice = 27n;
+
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount] = await hre.viem.getWalletClients();
+
+    const bankie_deploy = await hre.viem.deployContract("Bankie", [adoptionPrice], {
+      value: 0,
+    });
+    const bankie = await hre.viem.getContractAt("Bankie", bankie_deploy.address)
+    const publicClient = await hre.viem.getPublicClient();
+
+    return {
+      bankie,
+      adoptionPrice,
+      owner,
+      otherAccount,
+      publicClient,
+    };
+  }
+
+async function deployAndMint(){
+const { bankie, adoptionPrice, otherAccount } = await loadFixture(deployBankie);
+
+      const tokenURI = "ipfs://test-uri";
+      // Call adopt from otherAccount
+      const tx = await bankie.write.adopt(
+        [otherAccount.account.address],
+        { account: otherAccount.account, value: adoptionPrice }
+      );
+
+      // Get the tokenId from the event or assume it's 0 for the first mint
+      const tokenId = 0n;
+
+      // Check owner of tokenId
+      const nftOwner = await bankie.read.ownerOf([tokenId]);
+  }
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+deployAndMint()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
